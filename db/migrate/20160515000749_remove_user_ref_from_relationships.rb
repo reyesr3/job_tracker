df6afa048dd43535f1dfeb0d3cfa2a0babca2370
Rich Whybrew <richwhybrew@gmail.com>
2016-05-18 13:17:30 -0700

    Added User Ref to Relationships table

--- db/migrate/20160515000749_remove_user_ref_from_relationships.rb
+++
@@ -1,6 +0,0 @@
-class RemoveUserRefFromRelationships < ActiveRecord::Migration
-  def change
-    remove_reference :relationships, :user, index: true
-    remove_foreign_key :relationships, :users
-  end
-end
