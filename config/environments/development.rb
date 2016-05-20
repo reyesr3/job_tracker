1d456ebcd94b9633a5f6df1ab5cbdf0d71ad06d2
Ryan Reyes <ryanreyes@Ryans-MacBook-Pro-2.local>
2016-05-20 16:05:56 -0700

    Added 'Forgot Password' email delivery

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,19 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: 'gmail.com',
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "rsgtracker",
+    password: "CodingDojo" 
+  }
 end
