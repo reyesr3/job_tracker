afd4c44b30509cfa48e61f7576f326e648060f01
samir <ssachdev13@gmail.com>
2016-05-21 16:20:53 -0700

    got rid of the wrong route /oauthcallback

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,6 @@ Rails.application.routes.draw do
   post '/calendars' => 'calendars#addevent'
   get '/calendars/googlelogin' => 'calendars#redirect'
   get '/oauthcallback' => 'calendars#callback'
-  get '/calendars' => 'calendars#calendars'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
